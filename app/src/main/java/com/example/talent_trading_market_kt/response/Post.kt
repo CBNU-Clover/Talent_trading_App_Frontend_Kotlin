811b7296774ce519ee2b19cc6c31ca90b2c41e4b
(shinhyo) <(tlsgyals1216@gmail.com)>
2023-05-14 07:41:36 +0900

    PostGellAllboard에 id 멤버 추가

--- app/src/main/java/com/example/talent_trading_market_kt/response/Post.kt
+++ app/src/main/java/com/example/talent_trading_market_kt/response/Post.kt
@@ -3,7 +3,7 @@ package com.example.talent_trading_market_kt.response
 import java.time.LocalDateTime
 
 class PostGetAllBoard {
-
+    var id:Long?=null
     var postName: String? = null
     var content: String? = null
 }
