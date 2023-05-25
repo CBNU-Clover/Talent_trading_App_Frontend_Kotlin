8ca7a1f1815f031f31c9df92da16f51a20c73583
(shinhyo) <(tlsgyals1216@gmail.com)>
2023-05-25 15:27:19 +0900

    게시판 글 클릭하고  결제페이지 버튼 누르면 결제 페이지가 나오는데 현재 포인트와 결제후 잔액 표시 기능 완성

--- app/src/main/java/com/example/talent_trading_market_kt/boardfunction/postsearch/SearchOneBoardActivity.kt
+++ app/src/main/java/com/example/talent_trading_market_kt/boardfunction/postsearch/SearchOneBoardActivity.kt
@@ -1,16 +1,21 @@
 
 package com.example.talent_trading_market_kt.boardfunction.postsearch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.talent_trading_market_kt.R
+import com.example.talent_trading_market_kt.payfunction.PayMentActivity
 import org.w3c.dom.Text
 
 class SearchOneBoardActivity : AppCompatActivity() {
     lateinit var writerNickname:TextView
     lateinit var title:TextView
     lateinit var content:TextView
+    lateinit var payment_button:Button
+    lateinit var board_price:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.one_search_board)
@@ -18,9 +23,17 @@ class SearchOneBoardActivity : AppCompatActivity() {
         writerNickname=findViewById(R.id.searchone_writer)
         title=findViewById(R.id.searchone_title)
         content=findViewById(R.id.searchone_content)
+        payment_button=findViewById(R.id.payment_button)
+        board_price=findViewById(R.id.board_price)
+        payment_button.setOnClickListener {
+            val intent=Intent(this,PayMentActivity::class.java)
+            intent.putExtra("pay_title",title.text)
+            intent.putExtra("pay_price",board_price.text)
+            startActivity(intent)
+        }
         if(intent.hasExtra("Search_writerNickname"))
         {
-            title.text=intent.getStringExtra("Search_writerNickname")
+            writerNickname.text=intent.getStringExtra("Search_writerNickname")
 
         }
         if(intent.hasExtra("Search_postName"))
